e6be4d25455079f21915036085fb2253e4337fcb
Tony Kharioki <antolisah@gmail.com>
2023-08-11 07:40:09 +0300

    add task functionality

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,10 +1,13 @@
 import "./Column.css";
-import { useMemo } from "react";
+import { useState } from "react";
 import { useStore } from "../store";
 import { shallow } from "zustand/shallow";
 import Task from "./Task";
 
 export default function Column ({ state }) {
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+
   const tasks = useStore(store =>
     store.tasks.filter(task => task.state === state),
     shallow
@@ -12,12 +15,35 @@ export default function Column ({ state }) {
 
   // const filtered = useMemo(() => tasks.filter(task => task.state === state), [tasks, state]); -- you can use this instead of shallow
 
+  const addTask = useStore(store => store.addTask);
+
   return (
     <div className="column">
-      <p>{state}</p>
+      <div className="titleWrapper">
+        <p>{state}</p>
+        <button onClick={() => setOpen(true)}>Add</button>
+      </div>
+
       {tasks.map(task => (
         <Task key={task.title} title={task.title} />
       ))}
+      {open && (
+        <div className="modal">
+          <div className="modalContent">
+            <input onChange={e => setText(e.target.value)} value={text} />
+            <button
+              onClick={() => {
+                addTask(text, state);
+                setText('');
+                setOpen(false);
+              }}
+            >
+              Add
+            </button>
+            <button onClick={() => setOpen(false)}>Cancel</button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
